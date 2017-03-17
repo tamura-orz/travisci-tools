8618cac8fdbbaa242053b2ccc48da53f672eb0ca
tamutamu <tamu.0.0.tamu@gmail.com>
2017-03-17 10:26:56 +0900

    Update travis.sh

--- common/travis.sh
+++ common/travis.sh
@@ -2,3 +2,9 @@ export REPO_USER=nablarch
 export DEVELOP_REPO_URL=http://ec2-52-220-174-87.ap-southeast-1.compute.amazonaws.com/artifactory/libs-nablarch-local
 export DEPLOY_HOST_URL=http://ec2-52-220-174-87.ap-southeast-1.compute.amazonaws.com/artifactory
 export DEPLOY_REPO_NAME=libs-nablarch-local
+
+git clone -b change-deploy-code https://github.com/tamura-orz/nablarch-gradle-plugin.git
+pushd nablarch-gradle-plugin
+chmod +x gradlew
+./gradlew install
+popd
