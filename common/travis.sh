6afa3698e767460f90376e44b01d0e7b031ba5da
tamutamu <tamu.0.0.tamu@gmail.com>
2017-04-10 11:49:48 +0900

    Update travis.sh

--- common/travis.sh
+++ common/travis.sh
@@ -6,7 +6,7 @@ export DEVELOP_REPO_NAME=libs-nablarch-local
 export DEPLOY_HOST_URL=http://ec2-52-220-174-87.ap-southeast-1.compute.amazonaws.com/artifactory
 export DEPLOY_REPO_NAME=libs-nablarch-local
 
-export S3_BACKET_URL=s3://tamutamu.test.s3
+export S3_BACKET_URL=s3://tamutamu.test.s3/repository
 
 git clone -b change-deploy-code https://github.com/tamura-orz/nablarch-gradle-plugin.git
 pushd nablarch-gradle-plugin
