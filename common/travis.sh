2a87157f9f0f6a4bb3f130106447ad3c414bc2ff
tamutamu <tamu.0.0.tamu@gmail.com>
2017-04-10 16:27:02 +0900

    Update travis.sh

--- common/travis.sh
+++ common/travis.sh
@@ -1,7 +1,7 @@
 export REPO_USER=nablarch
 
-export DEVELOP_REPO_URL=http://ec2-52-220-174-87.ap-southeast-1.compute.amazonaws.com/artifactory
-export DEVELOP_REPO_NAME=libs-nablarch-local
+export DEVELOP_REPO_URL=http://tamutamu.test.s3.s3-website-ap-southeast-2.amazonaws.com
+export DEVELOP_REPO_NAME=repository
 
 export DEPLOY_HOST_URL=s3://tamutamu.test.s3
 export DEPLOY_REPO_NAME=repository
